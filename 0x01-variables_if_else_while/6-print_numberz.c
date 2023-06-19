89e27b1c9a48004cf828b36d10ea2c9ef062e3b2
Minombreesvera <kehindebolaji25@gmail.com>
2023-06-19 12:22:55 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - To print numbers of base 10 starting from 0.
+*main - To print numbers of base 10 from 0.
 *
 *Return: 0
 */
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 23; i < 47; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
