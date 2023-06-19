b9ffc0912cfaf8e27315d7c3b038bc0eec45c8e0
Minombreesvera <kehindebolaji25@gmail.com>
2023-06-19 12:32:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - To print lower case alphabets in reverse.
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
