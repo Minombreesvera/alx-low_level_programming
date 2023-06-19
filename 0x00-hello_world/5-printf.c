3b5a7554dff3b0d2a3eaa56de2e4a1c666423c54
Minombreesvera <kehindebolaji25@gmail.com>
2023-06-19 15:10:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - To print a particular staement followed by a new line.
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
