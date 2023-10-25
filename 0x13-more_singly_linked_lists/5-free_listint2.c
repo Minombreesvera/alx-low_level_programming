05b437b7adc09d11bae20d306ca9894390adf1ce
Minombreesvera <kehindebolaji25@gmail.com>
2023-10-24 20:06:43 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
+		current = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
+	*head = NULL
 }
